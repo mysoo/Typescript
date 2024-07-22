89a67eb1dcda0d5f8cdb4e8b040b56c15d5b8522
SoohyunMyung <soohyun.myung@qcells.com>
2024-07-22 15:39:28 +0900

    Feat: "section1-3 ts to js"

--- src/thirdType.ts
+++ src/thirdType.ts
@@ -1,27 +1,26 @@
 //ts->js로 변환 시 사라지는 타입 정의 방법 4가지
 
 //방법1: 콜론
-const val:true=true;
+const val: true = true;
 
 //방법2: type
-type Add1 = ()=> number;
+type Add1 = () => number;
 
 //방법3: interface
-interface Minus1{}
+interface Minus1 {}
 
 //방법4: generic
-Array<string>
-
+Array<string>;
 
 //방법5: body없는 function (심화)
 //타입 정의
-function add3(x:number, y:number):number
+function add3(x: number, y: number): number;
 //함수 선언
-function add3(x:number,y:number){
-    return x+y;
+function add3(x: number, y: number) {
+  return x + y;
 }
-add3(4,5)
+add3(4, 5);
 
 //방법6: as라는 특별한 키워드. 앞에 타입을 강제로 바꿈. 해당 부분도 js에서는 사라지는 부분!
-let aa=123;
-aa='hello'as unknown as number;
+let aa = 123;
+aa = "hello" as unknown as number;
